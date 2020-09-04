3aa77de0d9316fda9e98123adf19c7a5baf65e21
Sebastian <Sebastian@Strobl.net>
2020-09-04 17:35:30 +0200

    Update: dependencies

--- client/lib/app_component.dart
+++ client/lib/app_component.dart
@@ -1,7 +1,5 @@
 import 'package:angular/angular.dart';
 
-import 'src/todo_list/todo_list_component.dart';
-
 // AngularDart info: https://angulardart.dev
 // Components info: https://angulardart.dev/components
 
@@ -9,7 +7,7 @@ import 'src/todo_list/todo_list_component.dart';
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  directives: [],
 )
 class AppComponent {
   // Nothing here yet. All logic is in TodoListComponent.
