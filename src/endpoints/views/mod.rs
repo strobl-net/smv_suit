54a297e570d8a85d4e5495d4ca6b7f4a5a46a3c9
Sebastian <Sebastian@Strobl.net>
2020-09-27 16:07:33 +0200

    Add: API endpoint templates

--- src/endpoints/views/mod.rs
+++ src/endpoints/views/mod.rs
@@ -1,10 +1,12 @@
 use actix_web::web::ServiceConfig;
-use actix_web::{get, web, HttpResponse, Error};
+use actix_web::{get, web, HttpResponse, Error, HttpRequest};
 
 mod persons;
 
 pub fn endpoints(config: &mut ServiceConfig) {
-    config.service(index);
+    config
+        .service(index)
+        .service(api);
     persons::endpoints(config);
 }
 
@@ -13,4 +15,18 @@ pub async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
     let mut ctx = tera::Context::new();
     let body = tmpl.render("index.html", &ctx).unwrap();
     Ok(HttpResponse::Ok().content_type("text/html").body(body))
+}
+
+#[get("/api")]
+pub async fn api(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    let body = tmpl.render("api.html", &ctx).unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(body))
+}
+
+pub fn extract_path(req: &HttpRequest) -> (&str, Vec<&str>) {
+    let mut path: Vec<&str> = req.path().split("/").collect();
+    path.drain(0..1);
+    let title: &str = path.last().unwrap();
+    (title, path)
 }
--- src/endpoints/views/persons.rs
+++ src/endpoints/views/persons.rs
@@ -1,5 +1,6 @@
 use actix_web::web::ServiceConfig;
-use actix_web::{web, Error, HttpResponse, get};
+use actix_web::{web, Error, HttpResponse, get, HttpRequest};
+use crate::endpoints::views::extract_path;
 
 pub fn endpoints(config: &mut ServiceConfig) {
     config
@@ -7,8 +8,11 @@ pub fn endpoints(config: &mut ServiceConfig) {
 }
 
 #[get("/api/g/persons")]
-async fn all(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+async fn all(tmpl: web::Data<tera::Tera>, req: HttpRequest) -> Result<HttpResponse, Error> {
     let mut ctx = tera::Context::new();
+    let (title, path) = extract_path(&req);
+    ctx.insert("pages", &path);
+    ctx.insert("title", &title);
     let body = tmpl.render("models/person.html", &ctx).unwrap();
     Ok(HttpResponse::Ok().content_type("text/html").body(body))
 }
